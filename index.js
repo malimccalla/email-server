6f6595494e7935605b0f6c97aff57119822fcfac
Mali Michael <malimccalla@gmail.com>
2017-07-31 20:16:00 +0100

    Adds google config

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+
+passport.use(new GoogleStrategy());
 
 const app = express();
-
-app.get('/', (req, res) => {
-  res.send({ bye: 'buddy yo' });
-});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
