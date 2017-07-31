8b9383ff9ccfb2aece57d29c414d421dede69472
Mali Michael <malimccalla@gmail.com>
2017-07-31 22:22:06 +0100

    Facebook login

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const FacebookStrategy = require('passport-facebook').Strategy;
 const keys = require('./config/keys');
 
 const app = express();
+
+// GOOGLE
 
 passport.use(
   new GoogleStrategy(
@@ -28,6 +31,33 @@ app.get(
 );
 
 app.get('/auth/google/callback', passport.authenticate('google'));
+
+// FACEBOOK
+
+passport.use(
+  new FacebookStrategy(
+    {
+      clientID: keys.facebookClientID,
+      clientSecret: keys.facebookClientSecret,
+      callbackURL: '/auth/facebook/callback',
+      profileFields: ['email', 'id', 'displayName', 'name', 'gender'],
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log('Acess token', accessToken);
+      console.log('refreshToken', refreshToken);
+      console.log('profile', profile);
+    },
+  ),
+);
+
+app.get(
+  '/auth/facebook',
+  passport.authenticate('facebook', {
+    scope: ['public_profile', 'email', 'user_actions.music'],
+  }),
+);
+
+app.get('/auth/facebook/callback', passport.authenticate('facebook'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
