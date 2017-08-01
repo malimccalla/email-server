c9c765f67206a4ec783b977de52d5d8865a64c09
Mali Michael <malimccalla@gmail.com>
2017-08-01 15:30:06 +0100

    Fix trying to import dev keys for prod

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -10,19 +10,10 @@ module.exports = (app) => {
 
   app.get('/auth/google/callback', passport.authenticate('google'));
 
-  app.get(
-    '/auth/facebook',
-    passport.authenticate('facebook', {
-      scope: ['public_profile', 'email', 'user_actions.music'],
-    }),
-  );
-
   app.get('/api/logout', (req, res) => {
     req.logout();
     res.send(req.user);
   });
-
-  app.get('/auth/facebook/callback', passport.authenticate('facebook'));
 
   app.get('/api/current_user', (req, res) => {
     res.send(req.user);
--- services/passport.js
+++ services/passport.js
@@ -32,32 +32,3 @@ passport.use(
     },
   ),
 );
-
-// FACEBOOK
-
-passport.use(
-  new FacebookStrategy(
-    {
-      clientID: keys.facebookClientID,
-      clientSecret: keys.facebookClientSecret,
-      callbackURL: '/auth/facebook/callback',
-      profileFields: [
-        'email',
-        'id',
-        'displayName',
-        'name',
-        'gender',
-        'age_range',
-        'cover',
-        'locale',
-        'music',
-        'picture',
-      ],
-    },
-    (accessToken, refreshToken, profile, done) => {
-      console.log('Acess token', accessToken);
-      console.log('refreshToken', refreshToken);
-      console.log('profile', profile._json);
-    },
-  ),
-);
