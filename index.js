f6fd30f175b16ecfdb0baf16fdffd15bdc30f9ba
Mali Michael <malimccalla@gmail.com>
2017-08-03 13:14:51 +0100

    Add production routing logic

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
+const path = require('path');
 const bodyParser = require('body-parser');
 const keys = require('./config/keys');
 require('./models/user.model');
@@ -23,6 +24,14 @@ app.use(passport.session());
 
 require('./routes/auth.routes')(app);
 require('./routes/payments.routes')(app);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
