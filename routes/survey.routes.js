e90d1e9136d4942af56275217b77b21926057b36
Mali Michael <malimccalla@gmail.com>
2017-08-04 17:15:16 +0100

    Sets up sendgrid Mailer

--- routes/survey.routes.js
+++ routes/survey.routes.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/require_login');
 const requireCredits = require('../middlewares/require_credits');
+const Mailer = require('../services/Mailer');
+const surveyTemplate = require('../services/email_templates/surveyTemplate');
 
 const Survey = mongoose.model('surveys');
 
@@ -16,5 +18,7 @@ module.exports = app => {
       _user: req.user.id,
       dateSent: Date.now(),
     });
+
+    const mailer = new Mailer(survey, surveyTemplate(survey));
   });
 };
